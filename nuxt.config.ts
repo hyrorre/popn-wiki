fa9a8b921d39259f4590317580f48eeb53c54017
hyrorre <hiromu.yamashita@hotmail.com>
2026-03-29 10:11:34 +0900

    Fix: cloudflare binding config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -78,6 +78,25 @@ export default defineNuxtConfig({
   nitro: {
     experimental: {
       tasks: true
+    },
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true,
+      wrangler: {
+        d1_databases: [
+          {
+            binding: 'db',
+            database_name: 'popn-wiki-db',
+            database_id: process.env.NUXT_HUB_CLOUDFLARE_DATABASE_ID
+          }
+        ],
+        r2_buckets: [
+          {
+            binding: 'bucket',
+            bucket_name: process.env.NUXT_HUB_CLOUDFLARE_BLOB_BUCKET
+          }
+        ]
+      }
     }
   }
 })
