1d029ae698a4913fefd6d21b745de57ba5b6af8b
hyrorre <hiromu.yamashita@hotmail.com>
2026-04-16 12:43:30 +0900

    feat(ui): add color mode button

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,14 @@
 <template>
-  <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-    <p>hyrorre all rights reserved.</p>
-    <p class="mt-2">
-      <u-link to="https://github.com/hyrorre/popn-wiki" target="_blank">GitHub</u-link>
-      <u-link to="https://x.com/hyrorre" target="_blank" class="ml-2">Twitter</u-link>
-    </p>
-  </footer>
+  <u-footer>
+    <div class="text-center text-sm">
+      <p>hyrorre all rights reserved.</p>
+      <p class="mt-2">
+        <u-link to="https://github.com/hyrorre/popn-wiki" target="_blank">GitHub</u-link>
+        <u-link to="https://x.com/hyrorre" target="_blank" class="ml-2">Twitter</u-link>
+      </p>
+    </div>
+    <template #right>
+      <UColorModeButton />
+    </template>
+  </u-footer>
 </template>
