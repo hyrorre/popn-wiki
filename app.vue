5863a4e9eedbc7e74a0127bafc9dcd0492fe888e
hyrorre <hiromu.yamashita@hotmail.com>
2026-05-11 21:21:50 +0900

    feat(ui): add favicon

--- app.vue
+++ app.vue
@@ -17,7 +17,15 @@ useHead({
     { property: 'og:image', content: app.image },
     { name: 'twitter:card', content: app.card },
     { name: 'twitter:site', content: app.twitter },
-    { name: 'twitter:creator', content: app.twitter }
+    { name: 'twitter:creator', content: app.twitter },
+    { name: 'apple-mobile-web-app-title', content: 'ポップンWiki' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    { rel: 'shortcut icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/site.webmanifest' }
   ]
 })
 </script>
